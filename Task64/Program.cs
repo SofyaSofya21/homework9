3859a390ecba024445b93bca265e45e0e14ac8cc
Sofya <sofyaguseva1@gmail.comgit>
2022-12-10 01:10:42 +0100

    Tasks 64 and 66 completed

---
+++ Task64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int number = ReadInt("Введите число N: ");
+int minValue = 1;
+
+Console.Write($"N = {number} -> ");
+Console.WriteLine(Numbers(number, minValue));
+
+int Numbers (int n, int min)
+{
+    if (min == n)
+        return min;
+    else
+        Console.Write(Numbers(n, min+1) + ", ");
+    return min;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ Task66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int numberM = ReadInt("Введите число M: ");
+int numberN = ReadInt("Введите число N: ");
+
+Console.WriteLine(SumBetweenElements(numberM, numberN));
+
+
+int SumBetweenElements(int m, int n)
+{
+    if (n > m)
+        return n + SumBetweenElements(m, n - 1);
+    else if (m > n)
+        return m + SumBetweenElements(n, m - 1);
+    else
+        return m;
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
